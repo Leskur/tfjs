7f5b47982165ab4240bea5b11d9f2c32ac3baf38
Leskut <O_o_Vampire@live.cn>
2019-08-11 13:35:40 +0800

    异步加载模型

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,13 @@
 const posenet = require('@tensorflow-models/posenet')
+
+
 //index.js
 
 Page({
   async onReady() {
     const camera = wx.createCameraContext(this)
     this.canvas = wx.createCanvasContext('pose', this)
-    this.net = await posenet.load({
-
-    })
-
+    this.loadPosenet()
     let count = 0
     const listener = camera.onCameraFrame((frame) => {
       count++
@@ -18,7 +17,16 @@ Page({
       }
 
     })
-    listener.start()
-
+    // listener.start()
   },
+  async loadPosenet() {
+    this.net = await posenet.load({
+      architecture: 'MobileNetV1',
+      outputStride: 16,
+      inputResolution: 193,
+      multiplier: 0.5,
+      // modelUrl: 'https://www.gstaticcnapps.cn/tfjs-models/savedmodel/posenet/mobilenet/float/050/model-stride16.json'
+    })
+    console.log(this.net)
+  }
 })
